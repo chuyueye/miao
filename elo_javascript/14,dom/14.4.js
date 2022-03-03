6532eb1046d77d66fa79a648fe0f72333a3ea56c
JACK <htyjack@gmail.com>
2022-03-03 22:10:46 +0800

    0303

--- elo_javascript/14,dom/14.4.js
+++ elo_javascript/14,dom/14.4.js
@@ -1,15 +1,25 @@
 function talksAbout(node, string) {
-    if (node.nodeType == Node.ELEMENT_NODE) {
-      for (let child of node.childNodes) {
-        if (talksAbout(child, string)) {
-          return true;
-        }
+  if (node.nodeType == Node.ELEMENT_NODE) {
+    for (let child of node.childNodes) {
+      if (talksAbout(child, string)) {
+        return true;
       }
-      return false;
-    } else if (node.nodeType == Node.TEXT_NODE) {
-      return node.nodeValue.indexOf(string) > -1;
     }
+    return false;
+  } else if (node.nodeType == Node.TEXT_NODE) {
+    return node.nodeValue.indexOf(string) > -1;
   }
-  
-  console.log(talksAbout(document.body, "book"));
-  // → true
+}
+
+console.log(talksAbout(document.body, "book"));
+// → true
+
+//0303
+let scriptA = Array.from(document.getElementsByTagName('script'))
+let ScriptA_inner = scriptA.map(s => s.innerHTML)
+console.log(ScriptA_inner)
+let scripA_source = scriptA.map(s =>s.src)
+console.log(scripA_source)
+console.log(ScriptA_inner||scripA_source)
+scriptA[16].innerHTML;
+console.dir(scriptA[16])
