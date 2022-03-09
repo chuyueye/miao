1911aa0328b445a7e2cfaa3a17fcfc3bbb6057b0
JACK <htyjack@gmail.com>
2022-03-09 22:16:49 +0800

    Update chuyueye-lodash.js

--- lodash/chuyueye-lodash.js
+++ lodash/chuyueye-lodash.js
@@ -5,6 +5,7 @@
  */
 
 let chuyueye = function () {
+    // how to iretate or call others.
     // do(repeat) something n times.
     let repeat = (n, action) => {
         for (let i = 0; i < n; i++) {
@@ -22,15 +23,27 @@ let chuyueye = function () {
         repeat((length - size),
             () => out2.push(out1.pop())
         )
-        return [out1,out2]
+        return [out1, out2]
     }
-    let compact = () => { return 1 }
+    //Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+    let compact = (array) => {
+        let localarray = array; //for pure function
+        let out = []; //initial the OUTPUT
+        for (let entry of localarray) {
+            if (entry == 0 || entry == null || Number.isNaN(entry)) { }
+            else (out.push(entry))
+        }
+        return out
+    }
+    let difference = (array, values) => {
+
+    }
 
     return {
         repeat: repeat,
         chunk: chunk,
         compact: compact,
-
+        difference: difference,
     }
 }()
 
