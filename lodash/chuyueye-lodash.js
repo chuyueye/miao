03ff1e62121996b5bfc877fc978a4e6cf8b76738
JACK <htyjack@gmail.com>
2022-03-09 21:53:24 +0800

    0309

--- lodash/chuyueye-lodash.js
+++ lodash/chuyueye-lodash.js
@@ -4,13 +4,32 @@
  * @param 
  */
 
-let chuyueye = function(){
-    chunk(){}
-    compact(){}
-    
+let chuyueye = function () {
+    // do(repeat) something n times.
+    let repeat = (n, action) => {
+        for (let i = 0; i < n; i++) {
+            action()
+        }
+    }
+    //Creates an array of elements split into groups the length of size.
+    let chunk = (array, size = 1) => {
+        let out1 = array;
+        let out2 = [];
+        let length = array.length;
+        if (length <= size) {
+            return [array]
+        }
+        repeat(length - size,
+            () => out2.push(out1.pop())
+        )
+        return [out1,out2]
+    }
+    let compact = () => { return 1 }
+
     return {
-        chunk:chunk,
-        compact:compact,
+        repeat: repeat,
+        chunk: chunk,
+        compact: compact,
 
     }
 }()
