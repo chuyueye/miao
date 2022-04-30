be6927af986eef219c76f3559d3df7ee6aafbc59
JACK <htyjack@gmail.com>
2022-04-30 22:41:40 +0800

    Create jQuery.js

---
+++ jQuery/jQuery.js
@@ -0,0 +1,23 @@
+
+
+
+jQuery.each( [ "get", "post" ], function( _i, method ) {
+	jQuery[ method ] = function( url, data, callback, type ) {
+
+		// Shift arguments if data argument was omitted
+		if ( typeof data === "function" ) {
+			type = type || callback;
+			callback = data;
+			data = undefined;
+		}
+
+		// The url can be an options object (which then must have .url)
+		return jQuery.ajax( jQuery.extend( {
+			url: url,
+			type: method,
+			dataType: type,
+			data: data,
+			success: callback
+		}, jQuery.isPlainObject( url ) && url ) );
+	};
+} );
