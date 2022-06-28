8f996a778399b179a2e37d594bb6863f061cb90b
JACK <htyjack@gmail.com>
2022-06-28 22:41:43 +0800

    1

--- daily/react/my-app/src/index.js
+++ daily/react/my-app/src/index.js
@@ -4,54 +4,82 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         value: null,
-    //     }
-    // }
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-                {/* change the value from props to state */}
-            </button>
-        );
-    }
+// class Square extends React.Component {
+//     // constructor(props) {
+//     //     super(props);
+//     //     this.state = {
+//     //         value: null,
+//     //     }
+//     // }
+//     render() {
+//         return (
+//             <button
+//                 className="square"
+//                 onClick={() => this.props.onClick()}
+//             >
+//                 {this.props.value}
+//                 {/* change the value from props to state */}
+//             </button>
+//         );
+//     }
+// }
+
+function Square(props) {
+    // console.log(this) //undefined index.js:28 
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
 
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-        }
-    }
-    handleClick(i) {
-        const squares = this.state.squares.slice(); // make copy of slice()
-        squares[i] = 'X';
-        this.setState({ squares: squares })
-    }
+    // constructor(props) {
+    //     super(props); //React.Component.constructor
+    //     this.state = {
+    //         squares: Array(9).fill(null),
+    //         xIsNext: true, // first move default by X,each time changes
+    //     }
+    // }
+    // handleClick(i) {
+    //     const squares = this.state.squares.slice(); // make copy of slice() from Board.state
+    //     if (calculateWinner(squares) || squares[i]) {
+    //         return;
+    //     }
+    //     squares[i] = this.state.xIsNext ? 'X' : 'O';
+    //     // array squares is local varible of the function "handleClick"
+    //     // this means the Board Component
+    //     this.setState({
+    //         squares: squares, // with copy,override the original state.squares
+    //         xIsNext: !this.state.xIsNext,
+    //     });
+    // }
     renderSquare(i) {
+        // passing arguments(by properties)
+        // there is no render method of "Square" so far
         return (
             <Square
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
 
     render() {
-        const status = 'Next player: X';
-        //   const a = 1;
+        // const winner = calculateWinner(this.state.squares);
+        // let status;
+
+        // if (winner) {
+        //     status = 'Winner: ' + winner;
+        //     // if (winner === 'Tie!') { status = 'Tie!' }; // Tier! no 'Winner'
+        // } else {
+        //     status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        // }
+        // //   const a = 1;
 
         return (
             <div>
-                <div className="status">{status}</div>
+                {/* <div className="status">{status}</div> */}
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -74,21 +102,88 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        }
+    }
+    handleClick(i) {
+        const history = this.state.history;
+        console.log(history.length);
+        const current = history[history.length - 1];
+        // console.log(current);
+        const squares = current.squares.slice(); // make copy of slice() from state
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]), // with copy,override the original 
+            xIsNext: !this.state.xIsNext,
+        });
+    }
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
+                    <div>{status}</div>
                     <ol>{/* TODO */}</ol>
+                </div>
+                <div>
+                    <Reload />
                 </div>
             </div>
         );
     }
 }
-
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    if (squares.indexOf(null) === -1) { return 'Tie!' }
+    return null;
+}
+function Reload() {
+    return (
+        <button onClick={() => { window.location.reload() }}>
+            "Reload"
+        </button>
+    )
+}
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
