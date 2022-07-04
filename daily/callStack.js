9e8a564739d7e098d7ea35d38612d8509c927548
JACK <htyjack@gmail.com>
2022-07-04 22:04:51 +0800

    11

--- daily/callStack.js
+++ daily/callStack.js
@@ -1,11 +1,22 @@
-function baz(z){
+function baz(z) {
     console.log(new Error().stack)
 }
-function bar(y){
-    baz(y+1)
+function bar(y) {
+    baz(y + 1)
 }
-function foo(x){
-    bar(x+1)
+function foo(x) {
+    bar(x + 1)
 }
 foo(3)
-return
+// return
+
+const p = Promise.resolve('hello');
+
+let retu = p
+    .then(val => {
+        console.log(val)
+        return `${val} world`
+    })
+    .then(newVal => {
+        console.log(newVal)
+    })
--- daily/httpServer/httpServer.js
+++ daily/httpServer/httpServer.js
@@ -4,8 +4,11 @@ const requestListener = function (req, res) {
     res.writeHead(200);
     // how to write html tags?
     // res.write();
-    res.write('Hello World!');
-    res.end();
+    res.write(`<html lang="en">\r\n`)
+    res.write(`<meta charset="utf-8">\r\n`)
+    res.write('<h1>Hello World!</h1>\r\n');
+    res.write(`<h2>${Date()}</h2>\r\n`);
+    res.end(`</html>`);
 }
 
 const server = http.createServer(requestListener);
